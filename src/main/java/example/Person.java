2ecf83629a4e9f548429e297f6330d91c4520b63
Filippo Ricca <filippo.ricca.disi@gmail.com>
2022-12-13 17:50:48 +0100

    initial commit

---
+++ src/main/java/example/Person.java
@@ -0,0 +1,13 @@
+package example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Person 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
